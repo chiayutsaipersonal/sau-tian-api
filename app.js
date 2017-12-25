e08b4c0dd4e8eb693726d6f8a1743ffdddfc781b
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2017-12-25 23:51:12 +0800

    refactor app.js and set start scripts

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-var express = require('express');
-var path = require('path');
-// var favicon = require('serve-favicon');
-var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+#!/usr/bin/env node
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const debug = require('debug')('sau-tian-api:server');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const http = require('http');
+const path = require('path');
+// const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+if (app.get('env') === 'development') app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -27,20 +30,61 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  let error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (error, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.locals.message = error.message;
+  res.locals.error = req.app.get('env') === 'development' ? error : {};
   // render the error page
-  res.status(err.status || 500);
+  res.status(error.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+// Get port from environment and store in Express
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Create HTTP server.
+const server = http.createServer(app);
+
+// Listen on provided port, on all network interfaces
+server.listen(port);
+
+// Event listener for HTTP server "error" event
+server.on('error', (error) => {
+  if (error.syscall !== 'listen') throw error;
+  let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
+
+// Event listener for HTTP server "listening" event
+server.on('listening', () => {
+  let addr = server.address();
+  let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+});
+
+// Normalize a port into a number, string, or false
+function normalizePort (val) {
+  let port = parseInt(val, 10);
+  if (isNaN(port)) return val; // named pipe
+  if (port >= 0) return port; // port number
+  return false;
+}
