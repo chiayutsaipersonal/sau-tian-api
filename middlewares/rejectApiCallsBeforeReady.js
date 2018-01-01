7793b61b1fb9fcf78aa503a27041929f86d59a68
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-01 11:02:47 +0800

    update middleware response code

--- middlewares/rejectApiCallsBeforeReady.js
+++ middlewares/rejectApiCallsBeforeReady.js
@@ -3,7 +3,7 @@ const db = require('../controllers/database')
 // check 'ready' state of live data, reject request if live data is not ready
 module.exports = (req, res, next) => {
   if (db.ready) return next()
-  res.status(404)
+  res.status(503)
   let error = new Error('Live data is not ready')
   return next(error)
 }
