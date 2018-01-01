c719382af3b4485a3fa59e65ee39360634f36c20
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-01 11:02:18 +0800

    update missing page handler

--- middlewares/pageNotFountHandler.js
+++ middlewares/pageNotFountHandler.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const logging = require('../controllers/logging')
 
 const protocol = require('../config/app').hosting.protocol
@@ -5,10 +7,9 @@ const domain = require('../config/app').hosting.domain
 const port = require('../config/app').hosting.port
 
 module.exports = (req, res, next) => {
-  let message = `Page requested is missing: ${protocol}://${domain}:${port}${req.originalUrl}`
-  let error = new Error(message)
-  logging.warning(message)
-  error.status = 404
-  res.status(error.status)
-  next(error)
+  logging.warning(`Page requested is missing: ${protocol}://${domain}:${port}${req.originalUrl}`)
+  return res
+    .status(404)
+    .type('text/html;charset=utf-8')
+    .sendFile(path.resolve('./dist/public/index.html'))
 }
