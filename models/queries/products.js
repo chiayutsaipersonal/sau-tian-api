64a7dd103b355a2c106abba307902941bb649a1b
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-09 01:08:33 +0800

    refactor query logic from routes

--- models/queries/products.js
+++ models/queries/products.js
@@ -1,14 +1,145 @@
+const fs = require('fs-extra')
+
 const db = require('../../controllers/database')
 const logging = require('../../controllers/logging')
 
+const notEqualTo = db.Sequelize.Op.ne
+
 module.exports = {
-  tableRecordCount: () => {
-    return db.sequelize
-      .query('SELECT products.* FROM products;')
-      .spread((data, meta) => Promise.resolve(data.length))
+  addConvFactorInfo,
+  backupConvFactorData,
+  extractConvFactorData,
+  findDuplicates,
+  getProduct,
+  getProducts,
+  recordCount,
+  removeConvFactorInfo,
+}
+
+// add conversion factor info to a product record
+function addConvFactorInfo ({ productId, conversionFactorId, conversionFactor }) {
+  let clearQuery = `UPDATE products SET conversionFactorId = NULL, conversionFactor = NULL WHERE id = '${productId}' OR conversionFactorId = '${conversionFactorId}';`
+  let updateQuery = `UPDATE products SET conversionFactorId = '${conversionFactorId}', conversionFactor = '${conversionFactor}' WHERE id = '${productId}'`
+  return db.sequelize.transaction(transaction => {
+    return db.sequelize.query(clearQuery, { transaction })
+      .then(() => db.sequelize.query(updateQuery, { transaction }))
       .catch(error => {
-        logging.error(error, 'modules/queries/products.tableRecordCount errored')
+        logging.error(error, 'modules/queries/products.addConvFactorInfo() errored')
         return Promise.reject(error)
       })
-  },
+  })
+}
+
+// backup conversionFactor data
+function backupConvFactorData (data) {
+  return fs
+    .outputJson('./data/disConFactor.json', data)
+    .then(() => Promise.resolve())
+    .catch(error => {
+      logging.error(error, 'modules/queries/products.backupConvFactorData() errored')
+      return Promise.reject(error)
+    })
+}
+
+// extract conversionFactor information from products table
+function extractConvFactorData () {
+  return db.Products
+    .findAll({
+      where: {
+        conversionFactorId: { [notEqualTo]: null },
+      },
+    })
+    .then(data => {
+      let mappedData = data.map(entry => {
+        return {
+          productId: entry.id,
+          conversionFactorId: entry.conversionFactorId,
+          conversionFactor: entry.conversionFactor,
+        }
+      })
+      return Promise.resolve(mappedData)
+    })
+    .catch(error => {
+      logging.error(error, 'modules/queries/products.extractConvFactorData() errored')
+      return Promise.reject(error)
+    })
+}
+
+// receives a productId and a conversionFactorId (3M productId)
+// query for a list of productId's which carries the same conversionFactorId
+// excluding the search target productId
+function findDuplicates (productId, conversionFactorId) {
+  return db.Products
+    .findAll({
+      where: {
+        id: { [notEqualTo]: productId },
+        conversionFactorId: conversionFactorId,
+      },
+    })
+    .then(data => Promise.resolve(data))
+    .catch(error => {
+      logging.error(error, 'modules/queries/products.findDuplicates() errored')
+      return Promise.reject(error)
+    })
+}
+
+// find a product instance
+function getProduct (productId) {
+  return db.Products
+    .findById(productId)
+    .then(productInstance => {
+      if (!productInstance) {
+        let error = new Error(`Specifiec product (id: '${productId}') does not exist`)
+        error.status = 400
+        return Promise.reject(error)
+      } else {
+        return Promise.resolve(productInstance)
+      }
+    })
+    .catch(error => {
+      logging.error(error, 'modules/queries/products.getProduct() errored')
+      return Promise.reject(error)
+    })
+}
+
+// get product listing ordered by productId, and does
+// server-side pagination if specified
+function getProducts (limit = null, offset = null) {
+  let options = { order: ['id'] }
+  if (limit && offset) {
+    options.limit = limit
+    options.offset = offset
+  }
+  return db.Products
+    .findAll(options)
+    .then(data => Promise.resolve(data))
+    .catch(error => {
+      logging.error(error, 'modules/queries/products.getProducts() errored')
+      return Promise.reject(error)
+    })
+}
+
+// query for current record count
+function recordCount () {
+  return db.sequelize
+    .query('SELECT id FROM products;')
+    .spread((data, meta) => Promise.resolve(data.length))
+    .catch(error => {
+      logging.error(error, 'modules/queries/products.recordCount() errored')
+      return Promise.reject(error)
+    })
+}
+
+// remove conversion factor information from specified product record
+function removeConvFactorInfo (productInstance) {
+  return productInstance
+    .update({
+      conversionFactorId: null,
+      conversionFactor: null,
+    })
+    .then(() => Promise.resolve())
+    .catch(error => {
+      logging.error(error, 'modules/queries/products.removeConvFactorInfo() errored')
+      return Promise.reject(error)
+    })
 }
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,4 @@
 const express = require('express')
-const { check } = require('express-validator/check')
-const { sanitize } = require('express-validator/filter')
-const fs = require('fs-extra')
-const multer = require('multer')
-
-const db = require('../controllers/database')
-const logging = require('../controllers/logging')
 
 const productQueries = require('../models/queries/products')
 
@@ -14,165 +7,81 @@ const pagination = require('../middlewares/pagination')
 const router = express.Router()
 
 router
-  // search for other products that carries the specified conversion factor id
+  // find a list of products that carries the same convFactorId
   .get('/:productId/conversionFactors/:conversionFactorId',
     (req, res, next) => {
-      return db.Products.findAll({
-        where: {
-          id: { [db.Sequelize.Op.ne]: req.params.productId },
-          conversionFactorId: req.params.conversionFactorId,
-        },
-      }).then(data => {
-        req.resJson = { data }
-        next()
-        return Promise.resolve()
-      }).catch(error => {
-        return next(error)
-      })
-    }
-  )
-  // GET product listing
-  .get('/',
-    pagination(productQueries.tableRecordCount),
-    (req, res, next) => {
-      let queryOptions = {
-        order: ['id'],
-      }
-      if (req.linkHeader) {
-        queryOptions.limit = req.queryOptions.limit
-        queryOptions.offset = req.queryOptions.offset
-      }
-      return db.Products
-        .findAll(queryOptions)
+      let a = req.params.productId
+      let b = req.params.conversionFactorId
+      return productQueries
+        .findDuplicates(a, b)
         .then(data => {
           req.resJson = { data }
           next()
           return Promise.resolve()
         })
-        .catch(error => {
-          return next(error)
+        .catch(error => next(error))
+    }
+  )
+  // get product listing (optional pagination)
+  .get('/',
+    pagination(productQueries.recordCount),
+    (req, res, next) => {
+      let query = req.linkHeader
+        ? productQueries.getProducts(req.queryOptions.limit, req.queryOptions.offset)
+        : productQueries.getProducts()
+      return query
+        .then(data => {
+          req.resJson = { data }
+          next()
+          return Promise.resolve()
         })
+        .catch(error => next(error))
     }
   )
   // clear conversion factor data from a specified product
-  .delete('/:productId', (req, res, next) => {
-    return db.Products
-      .findById(req.params.productId)
-      .then(product => {
-        if (!product) {
-          res.status(404)
-          let error = new Error(`Product id: '${req.params.productId} is missing'`)
-          return Promise.reject(error)
-        } else {
-          return product.update({
-            conversionFactorId: null,
-            conversionFactor: null,
-          }).catch(error => Promise.reject(error))
-        }
-      }).then(() => {
-        return db.Products
-          .findAll({
-            where: {
-              conversionFactorId: {
-                [db.Sequelize.Op.ne]: null,
-              },
-            },
-          })
-          .then(data => Promise.resolve(data.map(entry => {
-            return {
-              productId: entry.id,
-              conversionFactorId: entry.conversionFactorId,
-              conversionFactor: entry.conversionFactor,
-            }
-          })))
-          .catch(error => {
-            logging.error(error, 'Conversion factor data reading failure')
-            return Promise.reject(error)
-          })
-      }).then(records => {
-        return fs
-          .outputJson('./data/disConFactor.json', records)
-          .catch(error => {
-            logging.error(error, 'product conversion factor data writeout failure')
-            return Promise.reject(error)
-          })
-      }).then(() => {
-        req.resJson = { message: 'Conversion factor cleared...' }
-        next()
-        return Promise.resolve()
-      }).catch(error => next(error))
-  })
-  // update product record
-  .post('/',
-    multer().none(),
-    [
-      check('productId').isEmpty(),
-      check('conversionFactorId').isEmpty(),
-      check('conversionFactor').isEmpty().isFloat(),
-      sanitize('conversionFactor').toFloat(),
-    ],
+  // backup all convFactor data into a .json file
+  .delete('/:productId',
     (req, res, next) => {
-      return db.sequelize.transaction(transaction => {
-        return db.Products
-          .update({
-            conversionFactor: null,
-            conversionFactorId: null,
-          }, {
-            where: {
-              [db.Sequelize.Op.or]: [
-                { id: req.body.productId },
-                { conversionFactorId: req.body.conversionFactorId },
-              ],
-            },
-            transaction,
-          }).then(() => {
-            return db.Products.update({
-              conversionFactorId: req.body.conversionFactorId,
-              conversionFactor: req.body.conversionFactor,
-            }, {
-              where: { id: req.body.productId },
-              transaction,
-            })
-          }).then(() => {
-            return Promise.resolve()
-          }).catch(error => {
-            logging.error(error)
-            return Promise.reject(error)
-          })
-      }).then(() => {
-        return db.Products
-          .findAll({
-            where: {
-              conversionFactorId: {
-                [db.Sequelize.Op.ne]: null,
-              },
-            },
-          })
-          .then(data => Promise.resolve(data.map(entry => {
-            return {
-              productId: entry.id,
-              conversionFactorId: entry.conversionFactorId,
-              conversionFactor: entry.conversionFactor,
-            }
-          })))
-          .catch(error => {
-            logging.error(error, 'Conversion factor data reading failure')
-            return Promise.reject(error)
-          })
-      }).then(records => {
-        return fs
-          .outputJson('./data/disConFactor.json', records)
-          .catch(error => {
-            logging.error(error, 'product conversion factor data writeout failure')
-            return Promise.reject(error)
-          })
-      }).then(() => {
-        req.resJson = { message: '轉換率資料更新成功' }
-        next()
-        return Promise.resolve()
-      }).catch(error => {
-        return next(error)
-      })
+      return productQueries
+        .getProduct(req.params.productId)
+        .catch(error => {
+          if (error.status) res.status(error.status)
+          return Promise.reject(error)
+        }).then(productInstance => {
+          return productQueries.removeConvFactorInfo(productInstance)
+        }).then(() => {
+          return productQueries.extractConvFactorData()
+        }).then(convFactorData => {
+          return productQueries.backupConvFactorData(convFactorData)
+        }).then(() => {
+          req.resJson = { message: 'Conversion factor cleared...' }
+          next()
+          return Promise.resolve()
+        }).catch(error => next(error))
+    })
+  // update conversion factor data
+  // backup all convFactor data into a .json file
+  .post('/',
+    (req, res, next) => {
+      let productId = req.body.productId
+      let conversionFactorId = req.body.conversionFactorId
+      let conversionFactor = req.body.conversionFactor
+      return productQueries
+        .addConvFactorInfo(
+          productId,
+          conversionFactorId,
+          conversionFactor
+        ).then(() => {
+          return productQueries.extractConvFactorData()
+        }).then(convFactorData => {
+          return productQueries.backupConvFactorData(convFactorData)
+        }).then(() => {
+          req.resJson = { message: 'Conversion factor information updated' }
+          next()
+          return Promise.resolve()
+        }).catch(error => {
+          return next(error)
+        })
     }
   )
 
