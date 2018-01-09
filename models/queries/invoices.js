ab0cce6834353f56c4825882ae50fcc49bce9dae
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-09 11:12:57 +0800

    refactor query logic from invoices router module

--- models/queries/invoices.js
+++ models/queries/invoices.js
@@ -1,14 +1,91 @@
-// const fs = require('fs-extra')
+const fs = require('fs-extra')
 const uuidV4 = require('uuid/v4')
 
 const db = require('../../controllers/database')
 const logging = require('../../controllers/logging')
 
 module.exports = {
+  backupCustomInvoiceData,
+  extractCustomDataFromLiveData,
+  getCustomSalesData,
+  getCustomSalesRecord,
+  getIrreleventCustomData,
   getLiveData,
   prepCustomRecord,
+  recordCustomData,
 }
 
+// get full dataset from customSalesData table
+function getCustomSalesData () {
+  return db.CustomSalesData
+    .findAll()
+}
+
+// extract custom sales data from live invoice data
+function extractCustomDataFromLiveData (liveData) {
+  return Promise.resolve(liveData.map(entry => {
+    return {
+      id: entry.id,
+      invoiceId: entry.invoiceId,
+      clientId: entry.clientId,
+      salesId: entry.salesId,
+      productId: entry.productId,
+      conversionFactorId: entry.conversionFactorId,
+      unitPrice: entry.price,
+      _preserved: entry._preserved,
+      _clientId: entry._clientId,
+      _unitPrice: entry._price,
+      _quantity: entry._quantity,
+      _employeeId: entry._employeeId,
+    }
+  }))
+}
+
+// update or insert custom sales data record
+function recordCustomData (recordData) {
+  return db.CustomSalesData
+    .upsert(recordData)
+    .then(() => Promise.resolve())
+    .catch(error => {
+      logging.error(error, 'modules/queries/invoices.recordCustomData() errored')
+      return Promise.reject(error)
+    })
+}
+
+// get customSalesData record by id
+function getCustomSalesRecord (recordId) {
+  return db.CustomSalesData
+    .findById(recordId)
+    .then(recordInstance => {
+      if (!recordInstance) {
+        let error = new Error(`Specifiec customRecord (id: '${recordId}') does not exist`)
+        error.status = 400
+        return Promise.reject(error)
+      } else {
+        return Promise.resolve(recordInstance)
+      }
+    })
+    .catch(error => {
+      logging.error(error, 'modules/queries/invoices.getCustomSalesRecord() errored')
+      return Promise.reject(error)
+    })
+}
+
+// backup custom invoice data
+function backupCustomInvoiceData (data) {
+  return fs
+    .outputJson('./data/customSalesData.json', data)
+    .then(() => Promise.resolve())
+    .catch(error => {
+      logging.error(error, 'modules/queries/invoices.backupCustomInvoiceData() errored')
+      return Promise.reject(error)
+    })
+}
+
+// get live invoice data jointed with custom data
+// with data range specified
+// *** any custom data that does not match to live data will not be found!!! ***
+// *** e.g. when record from live source was changed ***
 function getLiveData (startDate, endDate) {
   return db.sequelize
     .query(liveDataQueryString(startDate, endDate))
@@ -19,6 +96,23 @@ function getLiveData (startDate, endDate) {
     })
 }
 
+// get custom invoice data that's outside of the specified date range
+function getIrreleventCustomData (startDate, endDate) {
+  let query = `
+    SELECT customSalesData.*
+    FROM customSalesData
+    INNER JOIN invoices ON customSalesData.invoiceId = invoices.id
+    WHERE invoices.date NOT BETWEEN '${startDate}' AND '${endDate}';`
+  return db.sequelize
+    .query(query)
+    .spread((data, meta) => Promise.resolve(data))
+    .catch(error => {
+      logging.error(error, 'modules/queries/invoices.getIrreleventCustomData() errored')
+      return Promise.reject(error)
+    })
+}
+
+// return live data query SQL string
 function liveDataQueryString (startDate, endDate) {
   return `
   SELECT invoices.date,
@@ -62,21 +156,30 @@ function liveDataQueryString (startDate, endDate) {
       date;`
 }
 
-function prepCustomRecord (submission) {
+// prep record data from request POST data
+// without empty fields
+function prepCustomRecord (reqSubmission) {
+  let requiredFields = ['id', 'invoiceId', 'clientId', 'salesId', 'productId', 'conversionFactorId', 'unitPrice']
+  requiredFields.forEach(field => {
+    if (customRecord[field] === undefined) {
+      let error = new Error(`Required field '${field}' is missing`)
+      return Promise.reject(error)
+    }
+  })
   let customRecord = {}
-  customRecord.invoiceId = submission.invoiceId
-  customRecord.clientId = submission.clientId
-  customRecord.salesId = submission.salesId
-  customRecord.productId = submission.productId
-  customRecord.conversionFactorId = submission.conversionFactorId
-  customRecord.unitPrice = submission.unitPrice
-  customRecord.id = submission.customSalesDataId !== null
-    ? submission.customSalesDataId
-    : uuidV4().toUpperCase()
-  if (submission._preserved !== null) customRecord._preserved = submission._preserved
-  if (submission._clientId !== null) customRecord._clientId = submission._clientId
-  if (submission.unitPrice !== null) customRecord._unitPrice = submission._unitPrice
-  if (submission._quantity !== null) customRecord._quantity = submission._quantity
-  if (submission._employeeId !== null) customRecord._employeeId = submission._employeeId
+  customRecord.id = !reqSubmission.customSalesDataId
+    ? uuidV4().toUpperCase()
+    : reqSubmission.customSalesDataId
+  customRecord.invoiceId = reqSubmission.invoiceId
+  customRecord.clientId = reqSubmission.clientId
+  customRecord.salesId = reqSubmission.salesId
+  customRecord.productId = reqSubmission.productId
+  customRecord.conversionFactorId = reqSubmission.conversionFactorId
+  customRecord.unitPrice = reqSubmission.unitPrice
+  if (reqSubmission._preserved !== undefined) customRecord._preserved = reqSubmission._preserved
+  if (reqSubmission._clientId !== undefined) customRecord._clientId = reqSubmission._clientId
+  if (reqSubmission.unitPrice !== undefined) customRecord._unitPrice = reqSubmission._unitPrice
+  if (reqSubmission._quantity !== undefined) customRecord._quantity = reqSubmission._quantity
+  if (reqSubmission._employeeId !== undefined) customRecord._employeeId = reqSubmission._employeeId
   return Promise.resolve(customRecord)
 }
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,130 +1,67 @@
 const express = require('express')
-const fs = require('fs-extra')
-const uuidV4 = require('uuid/v4')
 
 const db = require('../controllers/database')
-const logging = require('../controllers/logging')
+
+const invoiceQueries = require('../models/queries/invoices')
 
 const router = express.Router()
 
 router
-  .get('/', (req, res, next) => {
-    return db.sequelize
-      .query(liveDataQuery(req.query.startDate, req.query.endDate))
-      .spread((data, meta) => {
-        req.resJson = { data }
-        next()
-        return Promise.resolve(data.map(entry => {
-          return {
-            invoiceId: entry.invoiceId,
-            clientId: entry.clientId,
-            salesId: entry.salesId,
-            productId: entry.productId,
-            conversionFactorId: entry.conversionFactorId,
-            price: entry.price,
-            id: entry.id,
-            _preserved: entry._preserved,
-            _clientId: entry._clientId,
-            _price: entry._price,
-            _quantity: entry._quantity,
-            _employeeId: entry._employeeId,
+  // get live and matching custom sales data (within date range)
+  // extract valid custom sales data
+  // query for custom sales data outside of date range)
+  // back up custom sales data to .json file
+  // (valid data within date range plus all data outside of the range)
+  .get('/',
+    (req, res, next) => {
+      let dateRange = [req.query.startDate, req.query.endDate]
+      return invoiceQueries
+        .getLiveData(...dateRange)
+        .then(liveData => {
+          req.resJson = { data: liveData }
+          return Promise.resolve(liveData)
+        })
+        .then(liveData => invoiceQueries.extractCustomDataFromLiveData(liveData))
+        .then(validCustomData => {
+          return invoiceQueries
+            .getIrreleventCustomData(...dateRange)
+            .then(irreleventCustomData => {
+              let customSalesData = validCustomData.concat(irreleventCustomData)
+              return Promise.resolve(customSalesData)
+            })
+        })
+        .then(customSalesData => invoiceQueries.backupCustomInvoiceData(customSalesData))
+        .then(() => {
+          next()
+          return Promise.resolve()
+        })
+        .catch(error => next(error))
+    })
+  // record custom sales data
+  // read all custom sales data
+  // backup to .json file
+  // read the upserted record and return
+  .post('/',
+    (req, res, next) => {
+      return invoiceQueries
+        .prepCustomRecord(req.body)
+        .then(customRecord => {
+          return invoiceQueries
+            .recordCustomData(customRecord)
+            .then(() => invoiceQueries.getCustomSalesData())
+            .then(customSalesData => invoiceQueries.backupCustomInvoiceData(customSalesData))
+            .then(() => invoiceQueries.getCustomSalesRecord(customRecord.id))
+        })
+        .then(data => {
+          req.resJson = {
+            data,
+            message: 'Custom sales data recorded',
           }
-        }))
-        // return fs
-        //   .outputJson('./data/customSalesData.json', workingData)
-        //   .catch(error => {
-        //     logging.error(error, 'Custom sales data writeout failure')
-        //     return Promise.reject(error)
-        //   })
-        //   .then(() => {
-        //     req.resJson = { data }
-        //     next()
-        //     return Promise.resolve()
-        //   })
-      })
-      .catch(error => next(error))
-  })
-  .post('/', (req, res, next) => {
-    let workingData = {}
-    workingData.invoiceId = req.body.invoiceId
-    workingData.clientId = req.body.clientId
-    workingData.salesId = req.body.salesId
-    workingData.productId = req.body.productId
-    workingData.conversionFactorId = req.body.conversionFactorId
-    workingData.price = req.body.price
-    workingData.id = req.body.customSalesDataId !== null
-      ? req.body.customSalesDataId
-      : uuidV4().toUpperCase()
-    if (req.body._preserved !== null) workingData._preserved = req.body._preserved
-    if (req.body._clientId !== null) workingData._clientId = req.body._clientId
-    if (req.body._price !== null) workingData._price = req.body._price
-    if (req.body._quantity !== null) workingData._quantity = req.body._quantity
-    if (req.body._employeeId !== null) workingData._employeeId = req.body._employeeId
-    return db.CustomSalesData
-      .upsert(workingData)
-      .then(() => db.CustomSalesData.findAll())
-      .then(data => {
-        return fs
-          .outputJson('./data/customSalesData.json', data)
-          .catch(error => {
-            logging.error(error, 'Custom sales data writeout failure')
-            return Promise.reject(error)
-          })
-      })
-      .then(() => db.CustomSalesData.findById(workingData.id))
-      .then(data => {
-        req.resJson = {
-          data,
-          message: 'Custom sales data recorded',
-        }
-        next()
-        return Promise.resolve()
-      })
-      .catch(error => next(error))
-  }
+          next()
+          return Promise.resolve()
+        })
+        .catch(error => next(error))
+    }
   )
 
 module.exports = router
-
-function liveDataQuery (startDate, endDate) {
-  return `
-SELECT invoices.date,
-  products.name AS productName,
-  sales.price,
-  sales.quantity,
-  invoices.employeeId,
-  products.unit,
-  clients.name AS companyName,
-  clients.areaId,
-  products.conversionFactor,
-  invoices.id AS invoiceId,
-  clients.id AS clientId,
-  sales.id AS salesId,
-  products.id AS productId,
-  products.conversionFactorId,
-  products.sapId,
-  customSalesData.id AS customSalesDataId,
-  customSalesData._preserved,
-  customSalesData._clientId,
-  customSalesData._price,
-  customSalesData._quantity,
-  customSalesData._employeeId
-FROM invoices
-  INNER JOIN
-  clients ON clients.id = invoices.clientId
-  INNER JOIN
-  sales ON sales.invoiceId = invoices.id
-  INNER JOIN
-  products ON products.id = sales.productId
-  LEFT JOIN
-  customSalesData ON (customSalesData.invoiceId = invoices.id) AND
-                     (customSalesData.clientId = invoices.clientId) AND
-                     (customSalesData.salesId = sales.id) AND
-                     (customSalesData.productId = sales.productId) AND
-                     (customSalesData.conversionFactorId = products.conversionFactorId) AND
-                     (customSalesData.price = sales.price)
-WHERE invoices.date BETWEEN '${startDate}' AND '${endDate}' AND
-  products.conversionFactorId IS NOT NULL
-ORDER BY products.id,
-     date;`
-}
