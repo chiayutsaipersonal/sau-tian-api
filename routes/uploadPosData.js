2fefb9c0285a042a35c552a733e326e4700ff289
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-27 00:43:11 +0800

    route added a back door to upload live POS file for staging purposes

---
+++ routes/uploadPosData.js
@@ -0,0 +1,43 @@
+
+const del = require('del')
+const express = require('express')
+const fs = require('fs-extra')
+const path = require('path')
+const multer = require('multer')({
+  dest: path.resolve('./data'),
+  fileFilter: (req, file, cb) => {
+    if (!file.originalname.toLowerCase().match(/\.(dbf)$/)) {
+      return cb(new Error('Invalid files'), false)
+    }
+    return cb(null, true)
+  },
+})
+const Promise = require('bluebird')
+
+const logging = require('../controllers/logging')
+
+const router = express.Router()
+
+router
+  .post(
+    '/',
+    multer.array('livePosFiles', 4),
+    (req, res, next) => {
+      return Promise.each(req.files, file => {
+        return fs
+          .move(file.path, path.join(file.destination, file.originalname))
+          .then(() => {
+            logging.warning(`${file.originalname} is uploaded`)
+            return Promise.resolve()
+          })
+          .catch(error => {
+            logging(error, `${file.originalname} upload failure`)
+            return Promise.reject(error)
+          })
+      }).then(() => {
+        req.resJson = { message: 'Live POS data upload completed...' }
+        return next()
+      }).catch(error => next(error))
+    })
+
+module.exports = router
