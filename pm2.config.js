c918801f503f2001a9f5e58d4f062ea2e89ea97b
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-27 20:16:30 +0800

    update pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,12 +1,7 @@
-require('dotenv').config()
-
 module.exports = {
   apps: [{
     name: 'sau-tian-api',
     script: 'server.js',
     port: 9003,
-    watch: true,
-    error_file: 'error.log',
-    out_file: 'out.log',
   }],
 }
